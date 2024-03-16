7249530a636f629a1d2307c3890a26d499dcd7fd
Milan Petrovic <5rovicmilan8@gmail.com>
2024-03-16 02:17:06 +0100

    late night special

--- modules/stations.js
+++ modules/stations.js
@@ -1,4 +1,8 @@
 module.exports = {
+    Persona: {
+        url: '/home/tox/Music/Persona 5 - Beneath the Mask.mp3',
+        name: "Late Night Special",
+    },
     NaxiCaffe: {
         url: "https://naxidigital-cafe128ssl.streaming.rs:8022/;*.mp3",
         name: "Naxi Caffe"
