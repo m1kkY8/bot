9302a9e3bcd8153ba3d6c7d3cd694c70879f623b
Milan Petrovic <5rovicmilan8@gmail.com>
2024-03-24 20:17:56 +0100

    useragent

--- modules/play.js
+++ modules/play.js
@@ -1,6 +1,10 @@
 const { createAudioPlayer, joinVoiceChannel, createAudioResource, StreamType, NoSubscriberBehavior } = require('@discordjs/voice');
 const play = require('play-dl');
 
+play.setToken({
+    useragent: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+})
+
 async function play_youtube(message){
     
     const args = message.content.split(' ');
