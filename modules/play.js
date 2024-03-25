ec671f9515089c76db7c0c0ae7bd3a04dd5a1f8c
Milan Petrovic <5rovicmilan8@gmail.com>
2024-03-25 14:59:08 +0100

    removed cancer

--- modules/play.js
+++ modules/play.js
@@ -1,12 +1,6 @@
 const { createAudioPlayer, joinVoiceChannel, createAudioResource, StreamType, NoSubscriberBehavior } = require('@discordjs/voice');
 const play = require('synthara-streams');
 
-play.setToken({
-    youtube: {
-        cookie: "../youtube.com_cookies.txt",
-    }
-})
-
 async function play_youtube(message){
     
     const args = message.content.split(' ');
@@ -21,14 +15,14 @@ async function play_youtube(message){
         behaviors: NoSubscriberBehavior.Pause
     });
     
-    //const video_info = play.video_info(link, { Proxy: { Host: "socks5://192.252.215.5", Port: 16137 } } );
+    const video_info = play.video_info(link);
     const stream = await play.stream(link);
 
-//    const song = {
-//        title: video_info.video_details.title,
-//        duration: video_info.video_details.durationInSec
-//    }
-//
+    const song = {
+        title: video_info.video_details.title,
+        duration: video_info.video_details.durationInSec
+    }
+
     const audio_resource = createAudioResource(stream.stream, {inputType: stream.type});
     const channel = message.member.voice.channel;
 
@@ -45,12 +39,8 @@ async function play_youtube(message){
     connection.subscribe(player);
     player.play(audio_resource);
     
-    message.reply('svira');
+    message.reply(`${song.title}`);
 }
-
-play.setToken({
-    useragent: ["Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"],
-});
 
 module.exports = {
     command_name: 'play',
