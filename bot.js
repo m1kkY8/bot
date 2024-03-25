e0087e4abca0185f8e5a2808a652e134a5d854f1
Milan Petrovic <5rovicmilan8@gmail.com>
2024-03-25 16:42:50 +0100

    token

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const { Client, Events, GatewayIntentBits, Partials } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
-const { dev_token , prefix } = require('./config.json');
+const { token , prefix } = require('./config.json');
 
 const client = new Client({
     intents: [
@@ -51,4 +51,4 @@ client.once(Events.ClientReady, () => {
     console.log('dev bot');
 });
 
-client.login(dev_token);
+client.login(token);
